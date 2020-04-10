0e7441be13954c3340fe61653a5195b06e6666c0
Juha Myllari <jmyllari@gmail.com>
2020-04-10 21:27:38 +0300

    Files generated by PkgTemplates

---
+++ src/MiniPascal.jl
@@ -0,0 +1,5 @@
+module MiniPascal
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using MiniPascal
+using Test
+
+@testset "MiniPascal.jl" begin
+    # Write your own tests here.
+end
